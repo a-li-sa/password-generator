1f0220b2d290ddc1472423a1b41e0244ef9b23de
Alisa Poon <alisa.poon97@gmail.com>
2020-07-26 13:34:37 -0700

    if user does not confirm a character type, alert user to confirm at least one

--- script.js
+++ script.js
@@ -6,51 +6,57 @@ function writePassword() {
   //prompt character types to include in the password 
   var howLong = prompt("How long do you want your password to be?\n\nYour password must be at least 8 characters and no more than 128 characters");
   var pwLength = parseInt(howLong);
-  if (pwLength < 8 && pwLength > 128) {
-    alert("Pick another number");
-  }
-  //(lowercase, uppercase, numeric, and/or special characters)
-  var lowercase = confirm("Press OK to include lowercase letters.");
-  var uppercase = confirm("Press OK to include uppercase letters.");
-  var numeric = confirm("Press OK to include numbers.");
-  var special = confirm("Press OK to include special characters.");
+  if (pwLength >= 8 && pwLength <= 128) {
+    //(lowercase, uppercase, numeric, and/or special characters)
+    var lowercase = confirm("Press OK to include lowercase letters.");
+    var uppercase = confirm("Press OK to include uppercase letters.");
+    var numeric = confirm("Press OK to include numbers.");
+    var special = confirm("Press OK to include special characters.");
 
-  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-  var alphabetUpper = alphabet.split('');
-  var alphabetLower = alphabet.toLowerCase().split('');
-  var digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-  
-  var possibleCharacters = [];
+    var alphabetUpper = alphabet.split('');
+    var alphabetLower = alphabet.toLowerCase().split('');
+    var digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+    
+    var possibleCharacters = [];
 
-  if (lowercase) {
-    possibleCharacters.push(...alphabetLower);
-  }
-  if (uppercase) {
-    possibleCharacters.push(...alphabetUpper);
-  }
-  if (numeric) {
-    possibleCharacters.push(...digits);
-  }
-  if (special) {
-    possibleCharacters.push(...specialCharacters);
-  }
-
-  var randomCharacters = "";
-
-  function generatePassword() {
-    for (var i = 0; i < pwLength; i++) {
-      randomCharacters += possibleCharacters[Math.floor(Math.random() * (possibleCharacters.length))];
+    if (lowercase) {
+      possibleCharacters.push(...alphabetLower);
     }
-    return randomCharacters;
-  }
-  
-  var password = generatePassword();
-  
-  var passwordText = document.querySelector("#password");
+    if (uppercase) {
+      possibleCharacters.push(...alphabetUpper);
+    }
+    if (numeric) {
+      possibleCharacters.push(...digits);
+    }
+    if (special) {
+      possibleCharacters.push(...specialCharacters);
+    }
 
-  passwordText.value = password;
+    var randomCharacters = "";
+
+    function generatePassword() {
+      for (var i = 0; i < pwLength; i++) {
+        randomCharacters += possibleCharacters[Math.floor(Math.random() * (possibleCharacters.length))];
+      }
+      return randomCharacters;
+    }
+    
+    var password = generatePassword();
+
+    if (!lowercase & !uppercase & !numeric & !special) {
+      alert("You need to choose at least one character type");
+    } else {
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
+    }
+
+  } else {
+    //do not run the prompts if not 8 <= password length <= 128
+    alert("Your password must be at least 8 characters and no more than 128 characters.");
+  }
 
 }
 

5a989f91bb1b4ae8c1cde4e9839f33d82d5ea15a
Alisa Poon <alisa.poon97@gmail.com>
2020-07-26 13:15:32 -0700

    add variables

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  //prompt character types to include in the password 
+  var howLong = prompt("How long do you want your password to be?\n\nYour password must be at least 8 characters and no more than 128 characters");
+  var pwLength = parseInt(howLong);
+
+  //(lowercase, uppercase, numeric, and/or special characters)
+  var lowercase = confirm("Press OK to include lowercase letters.");
+  var uppercase = confirm("Press OK to include uppercase letters.");
+  var numeric = confirm("Press OK to include numbers.");
+  var special = confirm("Press OK to include special characters.");
+
+  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+  var alphabetUpper = alphabet.split('');
+  var alphabetLower = alphabet.toLowerCase().split('');
+  var digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specialCharacters = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
   
   var password = generatePassword();
   
